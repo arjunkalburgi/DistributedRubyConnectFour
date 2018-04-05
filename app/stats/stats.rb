6920a87e75479d88bf83e28c62a00a2a0506e649
Arjun Kalburgi <askalburgi@gmail.com>
2018-04-05 01:11:42 -0600

    stats contracts

--- app/stats/stats.rb
+++ app/stats/stats.rb
@@ -1,5 +1,58 @@
 require_relative './stats_contracts'
+require 'mysql'
+
 class Stats 
     include StatsContracts    
+
+  def initialize(host, user, pwd, db, port)
+    invariant 
+    pre_initialize(host, user, pwd, db, port)
+
+    @host = host
+    @user = user
+    @pwd = pwd
+    @db = db
+    @port = port
+    begin
+      @connection = Mysql.new(host, user, pwd, db, port)
+    rescue Mysql::Error => e
+      puts e.error
+    end
+
+    post_initialize
+    invariant
+  end
+
+  def add_stat
+    invariant 
+    pre_add_stat
+
+    post_add_stat
+    invariant
+  end
+
+  def get_game
+    invariant 
+    pre_get_game
+
+    post_get_game
+    invariant
+  end
+
+  def get_player
+    invariant 
+    pre_get_player
+
+    post_get_player
+    invariant
+  end
+
+  def get_league_stats
+    invariant 
+    pre_get_league_stats
+
+    post_get_league_stats
+    invariant
+  end
 
 end
--- app/stats/stats_contracts.rb
+++ app/stats/stats_contracts.rb
@@ -1,3 +1,51 @@
 module StatsContracts
 
+    def invariant
+    end
+
+    def pre_initialize(host, user, pwd, db, port)
+        raise "Database Creation Error, host must be String" host.is_a?(String)
+        raise "Database Creation Error, user must be String" user.is_a?(String)
+        raise "Database Creation Error, pwd must be String" pwd.is_a?(String)
+        raise "Database Creation Error, db must be String" db.is_a?(String)
+        raise "Database Creation Error, port must be String" port.is_a?(Fixnum)
+    end
+
+    def post_initialize(connection)
+        raise "Database Error, no db connection" !@connection.nil?
+    end
+
+    def pre_add_stat(game_id, p1, p2, winner)
+        raise "Database Input Error, input must be String" game_id.is_a?(String)
+        raise "Database Input Error, input must be String" p1.is_a?(String)
+        raise "Database Input Error, input must be String" p2.is_a?(String)
+        raise "Database Input Error, input must be String" winner.is_a?(String)
+    end
+
+    def post_add_stat
+        # Stat should be added to database
+    end
+
+    def pre_get_game(game_id)
+        raise "Database Input Error, input must be String" game_id.is_a?(String)
+    end
+
+    def post_get_game(result)
+        raise "Database Input Error, result must be String" result.is_a(String)
+    end
+
+    def pre_get_player(id)
+        raise "Database Input Error, input must be String" game_id.is_a?(String)
+    end
+
+    def post_get_player(result)
+        raise "Database Input Error, result must be String" result.is_a?(String)
+    end
+
+    def pre_league_stats
+    end
+
+    def post_league_stats(result)
+        raise "Database Input Error, result must be String" result.is_a?(String)
+    end
 end
